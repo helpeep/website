e923c91d0c35ac05c8f33568bc3c2b4b5b99b116
Jason Ingram <jsoningram@gmail.com>
2015-03-16 19:39:45 -0700

    fix background slides

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,5 @@
 
-var wallImage = document.getElementById("slide");
+var wallImage = document.getElementById("header");
 
 var imgArray = ["images/slide1.jpg", "images/slide2.jpg", 
 				"images/slide3.jpg", "images/slide4.jpg", 
@@ -7,7 +7,8 @@ var imgArray = ["images/slide1.jpg", "images/slide2.jpg",
 var imgIndex = 0;
 
 function changeImage () {
-	wallImage.setAttribute("src", imgArray[imgIndex]);
+	// wallImage.setAttribute("src", imgArray[imgIndex]);
+	wallImage.style.backgroundImage = "url(" + imgArray[imgIndex] + ")";
 	imgIndex++;
 
 	if( imgIndex >= imgArray.length ) {
