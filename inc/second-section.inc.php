223177fd63cfff8e620a07bbf184fa9dba5fb29a
tsubasaGoya <tsubasaGoya@users.noreply.github.com>
2015-04-15 21:54:12 -0700

    Update second-section.inc.php

--- inc/second-section.inc.php
+++ inc/second-section.inc.php
@@ -2,7 +2,7 @@
 			<section id="second" class="main">
 				<header>
 					<div class="container">
-						<iframe width="100%" height="100%"
+						<iframe width="100%" height="645"
 src="https://www.youtube.com/embed/fpJFStw5rjY">
 </iframe>
 					</div>
