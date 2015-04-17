7fa0f71216a6853fdb14ec5fa4ed6c86439371a2
tsubasaGoya <tsubasaGoya@users.noreply.github.com>
2015-04-16 22:59:39 -0700

    Update second-section.inc.php

--- inc/second-section.inc.php
+++ inc/second-section.inc.php
@@ -4,8 +4,8 @@
 					<div class="container">
 						<iframe width="100%" height="645"
 src="https://www.youtube.com/embed/fpJFStw5rjY">
-</iframe><br><strong>
-<a href= "https://cash.me/$Helpeep">cash.me/$Helpeep</a></strong>			</div>
+</iframe><div></div>
+<strong><a href= "https://cash.me/$Helpeep">cash.me/$Helpeep</a></strong>			</div>
 				</header><!-- hidden for now
 				<div class="content dark style2">
 					<div class="container">
